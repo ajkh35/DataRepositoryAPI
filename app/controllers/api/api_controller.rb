7633c954a1f980b106e9b988e2ccdc24c8f4ed86
ajkh35 <ajay35@live.in>
2018-03-02 05:28:16 +0100

    emptying the branch

--- app/controllers/api/api_controller.rb
+++
@@ -1,5 +0,0 @@
-module Api
-  class ApiController < ApplicationController
-    include DeviseTokenAuth::Concerns::SetUserByToken
-  end
-end
--- config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-
-  scope module: 'api', format: 'json' do
-    namespace 'v1' do
-      mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
-    end
-  end
-end
